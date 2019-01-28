53f942566fcc7a911e3fac329fe5e7d487ee539b
deepak <deepak@central.tech>
2019-01-28 18:34:05 +0700

    Fork() example based on 'silent:true' option.

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Some sample conceptual examples of Child Process topic in Node
 3. ExecFlile - implement basic example - DONE
 4. Fork - 
 4.1 - implement basic fork example - DONE
+4.2 - implement fork with silent=true - DONE
---
+++ src/fork_examples/useSilent.js
@@ -0,0 +1,26 @@
+/**
+ * By default, fork() creates a child process, where the child process inherits 'stdin', 'stdout' and 'stderror' from parent.
+ * It means whatever child process writes on its 'stdout', it is actually writing on parent's 'stdout'.
+ * Hence console displays the written data. And there is no need to intercept or listen on 'data' event listener of 'stdout' property of child process instance.
+ * 
+ * If fork() is used with 'silent:true', then child process's 'stdin', 'stdout' and 'stderror' are piped to the parent.
+ * This means parent process's output is disabled on console.
+ * These are then available on 'stdin', 'stdout' and 'stderror' streams on child process object and can be consumed via 'data' event listener. 
+ */
+
+const init = () => {
+
+    const cp = require('child_process')
+    const processObj = cp.fork(`${__dirname}/silentChild.js`, null, {
+        silent: true
+    })
+
+    if(processObj.stdout) {
+        console.log('Silent = True')
+        processObj.stdout.on('data', data => console.log(`${data}`))
+    }
+    else 
+    console.log('Silent = False')
+}
+
+module.exports = init
